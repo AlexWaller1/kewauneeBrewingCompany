9b967c045d820e24b62ddd93535f30e5e64bc1f8
Alex Waller <alex@artofalexwaller.com>
2022-03-02 21:44:42 -0500

    initialCommit

---
+++ index.js
@@ -0,0 +1,28 @@
+// home page
+console.log("Hello Oa");
+
+// game plan
+
+/* 
+
+Basic idea is that this is a website for a 
+restaurant/brewery 
+
+what we want for the site
+
+- a welcoming index page
+
+- user authentication
+
+- a menu where can order for pickup
+
+- a cart for the order 
+
+- a page that simulates a purchase
+
+- well designed with some smooth artwork
+
+- a weather api so customers can make informed
+decisions about outdoor seating
+
+*/
